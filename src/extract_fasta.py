cf16c15bc01c81307f4935cc0ceddfb97f5c5fe8
Ashley Yael Montiel Vargas <yaelmont@lcg.unam.mx>
2025-04-30 00:43:37 -0600

    fasta_archivos (funcion)

--- src/extract_fasta.py
+++ src/extract_fasta.py
@@ -147,6 +147,45 @@ def extraer_secuencias (tf_coordenadas, secuencia):
 
     return tf_secuencias
 
+def fasta_archivos(tf_secuencias, output_dir="TF_picos_fasta", chars_linea=80):
+    '''
+    Funcion que crea los archivos fasta para cada TF con todas las secuencias identificadas
+
+    Args:
+        tf_secuencias(dic): Diccionario con las secuencias por cada TF
+        output_dir (str): Directorio de salida
+    Return: 
+        Directorio con los archivos fasta generados
+    '''
+    #Crear directorio si no existe
+    os.makedirs(output_dir, exist_ok=True)
+
+    archivos_generados = []
+
+    for tf, secuencias in tf_secuencias.items():
+        #Si la secuencia esta vacia
+        if not secuencias:
+            continue
+
+        nombre_archivo = os.path.join(output_dir, f"{tf}.fa")
+        try:
+            #Generar un archivo oara escribir las secuencias
+            with open(nombre_archivo, 'w') as arch_salida:
+                for i, secuencia in enumerate(secuencias, 1):
+                    arch_salida.write(f">{tf}_pico_{i}_len={len(secuencia)}\n")
+                    
+                    # Escribir secuencia en líneas de 80 caracteres (estándar FASTA)
+                    for j in range(0, len(secuencia), chars_linea):
+                        arch_salida.write(f"{secuencia[j:j+chars_linea]}\n")
+
+                    archivos_generados.append(nombre_archivo)
+            print(f"Archivo generado: {nombre_archivo} ({len(secuencias)} secuencias)")
+            
+        except Exception as e:
+            print(f"Error generando archivo para {tf}: {str(e)}")
+    
+    return archivos_generados
+
 
 
 
