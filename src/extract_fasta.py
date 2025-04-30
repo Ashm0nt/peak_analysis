f64f6aa1377051314f4e4b3cd959ca9289fedcae
Ashley Yael Montiel Vargas <yaelmont@lcg.unam.mx>
2025-04-29 18:59:05 -0600

    Creación del script para el modulo de python

---
+++ src/extract_fasta.py
@@ -0,0 +1,7 @@
+'''
+Script para extraer secuencias FASTA de sitios de unión de factores de transcripción
+en E. coli sin usar módulos externos.
+
+Autor: Ashley Yael Montiel Vargas
+Fecha: 01-Mayo-2025
+'''
